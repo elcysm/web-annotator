7983622f8203a69ed0bba9da2bf75db79620a5ad
Ngoc Kha <khanguyen.jobs@gmail.com>
2022-08-11 17:45:25 +0700

    update login

--- app.py
+++ app.py
@@ -15,72 +15,29 @@ app = Flask(__name__)
 app.secret_key = "lethanhdat"
 dirname = os.getcwd()
 
+# Data Owner Custom 
+MAIL_USERNAME = 'webbasedannotator@gmail.com'
+MAIL_PASSWORD = 'eblqqukvfjguceyp'
+ROOT_DOMAIN = 'http://127.0.0.1:5000'
+EMAIL_SUBJECT = 'Thư mời đánh giá'
+
+#
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'webbasedannotator@gmail.com'
-app.config['MAIL_PASSWORD'] = 'eblqqukvfjguceyp'
+app.config['MAIL_USERNAME'] = MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.errorhandler(404)
-
 def not_found(e):
     return render_template('404.html')
-
-def generate_password():
-    #input the length of password
-    length = 16       
-
-    #define data
-    lower = string.ascii_lowercase
-    upper = string.ascii_uppercase
-    num = string.digits
-    # symbols = string.punctuation
-    #string.ascii_letters
-
-    #combine the data
-    all = lower + upper + num 
-
-    #use random 
-    temp = random.sample(all,length)
-
-    #create the password 
-    password = "".join(temp)
-    return password
-
-def generate_username():
-    
-    length = 6       
-
-    #define data
-    lower = string.ascii_lowercase
-    num = string.digits
-
-    #combine the data
-    all = lower + num
-
-    #use random 
-    temp = random.sample(all,length)
-
-    #create the password 
-    username = "".join(temp)
-    return 'user_' + username
-
-
-
-
 
 @app.before_request
 def make_session_permanent():
     session.permanent = True
     app.permanent_session_lifetime = timedelta(minutes=5)
-
-def check_role(user_role):
-    if user_role == '1':
-        return True
-    else:
-        return False
 
 @app.route('/')
 def index():
@@ -90,15 +47,14 @@ def index():
         success="")
     else:
         username = session['username']
-        connection = sqlite3.connect(dirname + '\data.db')
-        cursor = connection.cursor()
-        query1 = "SELECT role FROM user WHERE username = '{name}'".format(name = username)
-        cursor.execute(query1)
-        user_role = cursor.fetchone()[0]
-        if check_role(user_role)==True:
-            return redirect(url_for('admin_index'))
+        user_role = select_role(username)
+        if user_role != None:
+            if check_role(user_role[0])==True:
+                return redirect(url_for('admin_index'))
+            else:
+                return redirect(url_for('user_index'))
         else:
-            return redirect(url_for('user_index'))
+            return redirect(url_for('get_register')) 
         
 @app.route('/', methods=['POST'])
 def login():
@@ -106,14 +62,7 @@ def login():
     password = request.form['password']
     passwordhash = hashlib.md5(password.encode()).hexdigest()
     session['username'] = username
-
-    connection = sqlite3.connect(dirname + '\data.db')
-    cursor = connection.cursor()
-    query = "SELECT role FROM user WHERE username = '{name}' AND password = '{passw}'".format(name = username, passw = passwordhash)
-    cursor.execute(query)
-    result = cursor.fetchone()
-    connection.commit()
-
+    result = select_role_by_username_password(username, password)[1]
     if result != None:
         user_role =  result[0]
         if check_role(user_role)==True:
@@ -123,16 +72,11 @@ def login():
     else:
         return render_template('login.html', error="Sai tài khoản hoặc mật khẩu")
 
-        
 @app.route('/user')
 def user_index():
     if 'username' in session:
         username = session['username']
-        connection = sqlite3.connect(dirname + '\data.db')
-        cursor = connection.cursor()
-        query1 = "SELECT role FROM user WHERE username = '{name}'".format(name = username)
-        cursor.execute(query1)
-        user_role = cursor.fetchone()[0]
+        user_role = select_role(username)[0]
         if check_role(user_role)==False:
             return render_template('user.html', result=username, success="Đăng nhập thành công")
         else:
@@ -142,16 +86,11 @@ def user_index():
         error="",
         success="")
         
-
 @app.route('/admin')
 def admin_index():
     if 'username' in session:
         username = session['username']
-        connection = sqlite3.connect(dirname + '\data.db')
-        cursor = connection.cursor()
-        query1 = "SELECT role FROM user WHERE username = '{name}'".format(name = username)
-        cursor.execute(query1)
-        user_role = cursor.fetchone()[0]
+        user_role = select_role(username)[0]
         if check_role(user_role)==True:
             return render_template('admin.html', result=username, success="Đăng nhập thành công")
         else:
@@ -161,15 +100,13 @@ def admin_index():
         error="",
         success="")
 
-
 @app.route('/admin/invitation', methods=['POST'])
 def post_invitation():
-    
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
 
-    connection = sqlite3.connect(dirname + '\data.db')
+    connection = connect_to_db()
     cursor = connection.cursor()
 
     passwordhash = hashlib.md5(password.encode()).hexdigest()
@@ -177,34 +114,26 @@ def post_invitation():
     cursor.execute(query1)
     connection.commit()
 
-
     msg = Message(
-        'Thư mời đánh giá',
-        sender ='nguyenkha4123@gmail.com', 
-        recipients = [email, 'ngockha4123@gmail.com'],
+        EMAIL_SUBJECT,
+        sender = MAIL_USERNAME, 
+        recipients = [email],
         )
     
-    link = 'http://127.0.0.1:5000/login/username={username}&password={password}'.format(username=username, password=password)
+    link = ROOT_DOMAIN+'/login/username={username}&password={password}'.format(username=username, password=password)
     msg.html = render_template('welcome.html', username=username, password=password, link=link)
     mail.send(msg)
 
     return render_template('email_verify.html', email=email, success="Đăng kí thành công")
 
-
 @app.route('/login/username=<username>&password=<password>', methods=['GET'])
 def get_api_login(username, password):
     return render_template('login.html', username=username, password=password)
 
-
 @app.route('/login/username=<username>&password=<password>', methods=['POST'])
 def post_api_login(username, password):
-    connection = sqlite3.connect(dirname + '\data.db')
-    cursor = connection.cursor()
-    passwordhash = hashlib.md5(password.encode()).hexdigest()
-    query = "SELECT username FROM user WHERE username = '{name}' AND password = '{passw}'".format(name = username, passw = passwordhash)
-    cursor.execute(query)
-    result = cursor.fetchone()
-    connection.commit()
+    result = select_role_by_username_password(username, password)[0]
+
     if result != None:
         session['username'] = username 
         return redirect(url_for('user_index'))
@@ -219,11 +148,7 @@ def get_invitation():
         success="")
     else:
         username = session['username']
-        connection = sqlite3.connect(dirname + '\data.db')
-        cursor = connection.cursor()
-        query1 = "SELECT role FROM user WHERE username = '{name}'".format(name = username)
-        cursor.execute(query1)
-        user_role = cursor.fetchone()[0]
+        user_role = select_role(username)[0]
         if check_role(user_role)==True:
             username = generate_username()
             password = generate_password()
@@ -233,9 +158,6 @@ def get_invitation():
         else:
             return render_template('503.html')
 
-
-
-    
 @app.route('/login')
 def get_login():
     return redirect(url_for('index'))
@@ -245,11 +167,9 @@ def logout():
    session.pop('username', None)
    return redirect(url_for('index'))
 
-
 @app.route('/register', methods=['GET'])
 def get_register():
     return render_template('register.html', error="")
-
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -260,7 +180,7 @@ def register():
    if password != re_password:
         return render_template('register.html', error="Mật khẩu không khớp")
    else:
-        connection = sqlite3.connect(dirname + '\data.db')
+        connection = connect_to_db()
         cursor = connection.cursor()
         query1 = "SELECT * FROM user WHERE username = '{name}' OR email = '{email}'".format(name = username, email = email)
         cursor.execute(query1)
@@ -268,12 +188,60 @@ def register():
         connection.commit()
         if result == None:
             passwordhash = hashlib.md5(password.encode()).hexdigest()
-            query2 = "INSERT INTO user VALUES ('{name}','{passw}','{mail}')".format(name = username, mail = email, passw = passwordhash)
+            query2 = "INSERT INTO user VALUES ('{name}','{passw}','{mail}','1')".format(name = username, mail = email, passw = passwordhash)
             cursor.execute(query2)
             connection.commit()
             return render_template('email_verify.html', email=email, success="Đăng kí thành công")
         else:
             return render_template('register.html', error="Username hoặc email đã tồn tại!")
 
+# function
+def generate_password():
+    length = 16       
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    all = lower + upper + num
+    temp = random.sample(all,length)
+    password = "".join(temp)
+    return password
+
+def generate_username():
+    length = 6
+    lower = string.ascii_lowercase
+    num = string.digits
+    all = lower + num
+    temp = random.sample(all,length)
+    username = "".join(temp)
+    return 'user_' + username
+
+def check_role(user_role):
+    if user_role == '1':
+        return True
+    else:
+        return False
+
+def connect_to_db():
+    conn = sqlite3.connect(dirname + '\data.db')
+    return conn
+
+def select_role(username):
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    query1 = "SELECT role FROM user WHERE username = '{name}'".format(name = username)
+    cursor.execute(query1)
+    result = cursor.fetchone()
+    connection.commit()
+    return result
+
+def select_role_by_username_password(username, password):
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    passwordhash = hashlib.md5(password.encode()).hexdigest()
+    query = "SELECT username, role FROM user WHERE username = '{name}' AND password = '{passw}'".format(name = username, passw = passwordhash)
+    cursor.execute(query)
+    result = cursor.fetchone()
+    connection.commit()
+    return result
 
 app.run(debug=True)
